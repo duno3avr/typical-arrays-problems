ec7c3605a554a169326c2593a20eae6ccaed55b9
Burkun <duno3avr@gmail.com>
2020-02-20 19:11:31 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if (array === undefined){
+    return 0;
+  }
   return array.length > 0 ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
+  if (array === undefined){
+    return 0;
+  } 
   return (array.length > 0 ? Math.max(...array) : 0);
 }
 
 exports.avg = function avg (array) {
-  return array.reduce((acc, cur) => acc + cur) / array.length || 0;
+  if (array === undefined){
+    return 0;
+  } 
+  return array.reduce((acc, cur) => acc + cur, 0) / array.length || 0;
 }
